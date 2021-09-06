8765097b72bd368aa959cbea82948a6d04ed3fc1
sandaru <sandarubro95@gmail.com>
2021-09-06 20:14:12 +0530

    Conditional Rendering
    2 ways of rendering conditionally

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -3,30 +3,25 @@ import React, { Component } from "react";
 class Counter extends React.Component {
     state = {
         count: 1,
-        tags: ["tag1", "tag2", "tag3"],
+        tags: [],
     };
+    renderList() {
+        if (this.state.tags.length === 0) return <p>No tags to show here</p>;
+        return (
+            <ul>
+                {this.state.tags.map((tag) => (
+                    <li key={tag}>{tag}</li>
+                ))}
+            </ul>
+        );
+    }
     render() {
         return (
             <React.Fragment>
-                <span style={{ fontSize: 14 }} className={this.formatClasses()}>
-                    {this.formatCount()}
-                </span>
-                <button className="btn btn-secondary btn-sm">Increment</button>
-                <ul>
-                    {this.state.tags.map((tag) => (
-                        <li key={tag}>{tag}</li>
-                    ))}
-                </ul>
+                {this.state.tags.length === 0 && <h2>No tags to show here</h2>}
+                {this.renderList()}
             </React.Fragment>
         );
-    }
-    formatCount() {
-        return this.state.count === 0 ? "Zero" : this.state.count;
-    }
-    formatClasses() {
-        let normalClassText = "badge m-1 badge-";
-        return (normalClassText +=
-            this.state.count === 0 ? "warning" : "success");
     }
 }
 
