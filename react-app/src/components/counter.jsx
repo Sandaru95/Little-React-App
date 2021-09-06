cd3ab557f366772eb7039f5486ab78f18cec3faf
sandaru <sandarubro95@gmail.com>
2021-09-06 19:59:42 +0530

    Till How to Loop Through a list

--- react-app/src/components/counter.jsx
+++ react-app/src/components/counter.jsx
@@ -1,20 +1,33 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
 class Counter extends React.Component {
     state = {
-        count: 0,
+        count: 1,
+        tags: ["tag1", "tag2", "tag3"],
     };
-    styles = {
-
-    }
     render() {
         return (
             <React.Fragment>
-                <span className="badge badge-warning m-2">{this.state.count}</span>
+                <span style={{ fontSize: 14 }} className={this.formatClasses()}>
+                    {this.formatCount()}
+                </span>
                 <button className="btn btn-secondary btn-sm">Increment</button>
+                <ul>
+                    {this.state.tags.map((tag) => (
+                        <li key={tag}>{tag}</li>
+                    ))}
+                </ul>
             </React.Fragment>
         );
-    };
+    }
+    formatCount() {
+        return this.state.count === 0 ? "Zero" : this.state.count;
+    }
+    formatClasses() {
+        let normalClassText = "badge m-1 badge-";
+        return (normalClassText +=
+            this.state.count === 0 ? "warning" : "success");
+    }
 }
 
 export default Counter;
